16a0a9e4a5518a6441f00405c329d046d91f9d6b
Bunchhieng <bsoth@quickbase.com>
2017-12-08 22:19:48 -0500

    quizzler app

--- Quizzler-iOS11/Quizzler/ViewController.swift
+++ Quizzler-iOS11/Quizzler/ViewController.swift
@@ -23,7 +23,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let firstQuestion = allQuestions.list[currentIndex]
         questionLabel.text = firstQuestion.questionText
         scoreLabel.text = String(0)
@@ -33,38 +32,49 @@ class ViewController: UIViewController {
         if sender.tag == 1 {
             pickedAnswer = true
         } else if sender.tag == 2 {
-            pickedAnswer = true
+            pickedAnswer = false
         }
         
-        currentIndex = currentIndex + 1
         if pickedAnswer == checkAnswer(questionIndex: currentIndex) {
-            nextQuestion(currentIndex: currentIndex)
-            totalScore += 1
+            totalScore = totalScore + 1
             scoreLabel.text = String(totalScore)
         }
-        nextQuestion(currentIndex: currentIndex)
+        nextQuestion(ci: currentIndex)
+        updateUI(ci: currentIndex)
+        questionLabel.text = allQuestions.list[currentIndex].questionText
     }
     
-    func updateUI() {
-      
+    func updateUI(ci: Int) {
+        progressLabel.text = "\(ci + 1) / 13"
+        progressBar.frame.size.width = (view.frame.size.width/13) * CGFloat(ci + 1)
     }
     
-    func nextQuestion(currentIndex: Int) {
-        if currentIndex < (allQuestions.list.count - 1) {
-            let nextIndex = currentIndex + 1
-            questionLabel.text = allQuestions.list[nextIndex].questionText
+    // Go to the next question and show alert if reach the end of the list
+    func nextQuestion(ci: Int) {
+        // Check to make sure we don't have index out of range
+        if ci < (allQuestions.list.count - 1) {
+            // Show the next question
+            currentIndex = currentIndex + 1
+        } else {
+            self.startOver()
+            totalScore = 0
+            let alert = UIAlertController(title: "Awesome", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { (UIAlertActoin) in
+                
+            })
+            
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
         }
-        startOver(index: currentIndex)
     }
     
+    // Return the value of the answer
     func checkAnswer(questionIndex: Int) -> Bool {
         return allQuestions.list[questionIndex].answer
     }
     
-    func startOver(index: Int)  {
-        if index == (allQuestions.list.count - 1) {
-            currentIndex = 0
-            questionLabel.text = allQuestions.list[currentIndex].questionText
-        }
+    // Start from the beginning of the list
+    func startOver()  {
+        currentIndex = 0
     }
 }
